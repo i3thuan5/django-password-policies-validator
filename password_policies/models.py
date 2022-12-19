d33265599687a625bfcc25aa48d9c70c39731eed
niauah <khengiong@ithuan.tw>
2022-12-19 10:42:37 +0800

    Add models

--- password_policies/models.py
+++ password_policies/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class PasswordRecord(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        editable=False
+    )
+
+    # max_length 參考 django.contrib.auth.base_user.AbstractBaseUser.password
+    password = models.CharField(
+        'Password hash',
+        max_length=128,
+        editable=False
+    )
+    date = models.DateTimeField(
+        'Date',
+        auto_now_add=True,
+        editable=False
+    )
