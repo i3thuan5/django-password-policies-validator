c1e835045a96c7ee62dec2444ffdab04637831d5
niauah <khengiong@ithuan.tw>
2022-12-20 16:27:06 +0800

    Add signals

--- password_policies/apps.py
+++ password_policies/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PasswordPoliciesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'password_policies'
+
+    def ready(self):
+        # Implicitly connect signal handlers decorated with @receiver.
+        from . import signals
---
+++ password_policies/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+
+@receiver(post_save, sender=get_user_model())
+def my_callback(sender, **kwargs):
+    print('Signal connected')
