10a4018b8247ea72586eaa03d908581aaece6d41
Zuer <hong@ithuan.tw>
2022-12-21 16:49:04 +0800

    有改密碼才加record。

--- password_policies/signals.py
+++ password_policies/signals.py
@@ -1,8 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @receiver(post_save, sender=get_user_model())
 def create_password_record(sender, instance, **kwargs):
+    try:
+        if instance.password_records.latest('date').password == instance.password:
+            return
+    except ObjectDoesNotExist:
+        pass
     instance.password_records.create(password=instance.password)
