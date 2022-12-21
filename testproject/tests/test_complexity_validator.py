b0edc4115571c5e8c3d7673ce6aaba44f9b92cf8
Zuer <hong@ithuan.tw>
2022-12-21 10:56:54 +0800

    檢查字元siunn短ê試驗。

--- testproject/tests/test_complexity_validator.py
+++ testproject/tests/test_complexity_validator.py
@@ -55,3 +55,27 @@ class ComplexityValidatorTest(TestCase):
         self.assertFalse(
             userForm.has_error('password1') or userForm.has_error('password2')
         )
+
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {
+            'NAME': 'password_policies.password_validation.ComplexityValidator',
+            'OPTIONS':{
+                'min_char_types': 2,
+                'min_numeric_chars':5,
+                'min_uppercase_chars':0,
+                'min_lowercase_chars':2,
+                'min_symbol_chars':0,
+            },
+        }
+    ])
+    def test_length(self):
+        hana = User.objects.create_user('Hana')
+        userForm = UserCreationForm({
+            'username': 'Hana',
+            'password1': 'tomay123',
+            'password2': 'tomay123',
+        })
+        userForm.full_clean()
+        self.assertTrue(
+            userForm.has_error('password1') or userForm.has_error('password2')
+        )
