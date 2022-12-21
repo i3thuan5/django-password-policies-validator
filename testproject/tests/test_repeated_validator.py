d85d573c295c39bae4c80af355b4b91fd52241fc
Zuer <hong@ithuan.tw>
2022-12-21 14:03:52 +0800

    補options試驗。

--- testproject/tests/test_repeated_validator.py
+++ testproject/tests/test_repeated_validator.py
@@ -56,6 +56,11 @@ class RepeatedValidatorTest(TestCase):
         })
         self.assertFalse(password_form.is_valid())
 
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {
+            'NAME': 'password_policies.password_validation.RepeatedValidator',
+        }
+    ])
     def test_第5kái密碼會使khiāmkah第1kái_kāngkhuán_不可以與前三次使用過之密碼相同(self):
         user_form = UserCreationForm({
             'username': 'Hana',
@@ -88,7 +93,39 @@ class RepeatedValidatorTest(TestCase):
         })
         self.assertTrue(password_form.is_valid())
 
+
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {
+            'NAME': 'password_policies.password_validation.RepeatedValidator',
+            'OPTIONS': {
+                'record_length': 2,
+            }
+        }
+    ])
     def test_options設做2_第3kái密碼會使khiāmkah第1kái_kāngkhuán(self):
-        self.fail()
+        user_form = UserCreationForm({
+            'username': 'Hana',
+            'password1': 'tomay123',
+            'password2': 'tomay123',
+        })
+        user_form.full_clean()
+        hana = user_form.save()
+        form2 = SetPasswordForm(user=hana, data={
+            'new_password1': 'pawli456',
+            'new_password2': 'pawli456',
+        })
+        form2.full_clean()
+        form2.save()
+        form3 = SetPasswordForm(user=hana, data={
+            'new_password1': 'mali111',
+            'new_password2': 'mali111',
+        })
+        form3.full_clean()
+        form3.save()
+        password_form = SetPasswordForm(user=hana, data={
+            'new_password1': 'tomay123',
+            'new_password2': 'tomay123',
+        })
+        self.assertTrue(password_form.is_valid())
     def test_options設做0_ài錯誤(self):
         self.fail()
