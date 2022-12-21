f4b26e03175efdff212aa8db9acbd49b7581ba03
Zuer <hong@ithuan.tw>
2022-12-21 10:08:25 +0800

    test有法度走。

--- testproject/tests/test_complexity_validator.py
+++ testproject/tests/test_complexity_validator.py
@@ -1,10 +1,11 @@
-from unittest import TestCase
+from django.test import TestCase
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 
 
 class ComplexityValidatorTest(TestCase):
 	def test_lowercase_number(self):
 		hana = User.objects.create_user('Hana')
-		hana.set_password('tomay123')
-		with self.assertRaise('tomay123') as error:
+		with self.assertRaises(ValidationError, hana.set_password, 'tomay123') as error:
 			'大寫、小寫、數字、特殊符號中至少4種字元' in error.objects[0]
