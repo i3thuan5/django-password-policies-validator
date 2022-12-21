e2fa2ebd7457c1038cc4183d177aab42411e762c
Zuer <hong@ithuan.tw>
2022-12-21 14:49:30 +0800

    試驗寫好，會用得。

--- testproject/tests/test_minimum_reset_interval_validator.py
+++ testproject/tests/test_minimum_reset_interval_validator.py
@@ -55,5 +55,34 @@ class MinimumResetIntervalValidatorTest(TestCase):
                 'new_password2': 'pawli456',
             })
             self.assertTrue(password_form.is_valid())
+
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {
+            'NAME':
+            'password_policies.password_validation.MinimumResetIntervalValidator',
+            'OPTIONS':{
+                'min_interval_days': 2.0,
+            }
+        }
+    ])
     def test_options設做2_過1工後改密碼_會擋起來(self):
-        self.fail()
+        with patch(
+            'django.utils.timezone.now',
+            return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
+        ):
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
+        with patch(
+            'django.utils.timezone.now',
+            return_value=datetime(2022, 12, 22, tzinfo=timezone.utc)
+        ):
+            password_form = SetPasswordForm(user=hana, data={
+                'new_password1': 'pawli456',
+                'new_password2': 'pawli456',
+            })
+            self.assertFalse(password_form.is_valid())
