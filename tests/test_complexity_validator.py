6b918484e80dba42d180b5039f3d96689f26830c
Zuer <hong@ithuan.tw>
2022-12-21 09:33:16 +0800

    Patongal to sakacecay a kinsa.

---
+++ tests/test_complexity_validator.py
@@ -0,0 +1,8 @@
+
+
+class ComplexityValidatorTest(TestCase):
+	def test_lowercase_number(self):
+		hana = User.objects.create_user('Hana')
+		hana.set_password('tomay123')
+		with self.assertRaise('tomay123') as error:
+			'大寫、小寫、數字、特殊符號中至少4種字元' in error.objects[0]
