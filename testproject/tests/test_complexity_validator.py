9fe511a55e3250e35dffed8ce6371bcaf7ed7ec6
Zuer <hong@ithuan.tw>
2022-12-21 10:55:32 +0800

    ka-tī調設定ê試驗。

--- testproject/tests/test_complexity_validator.py
+++ testproject/tests/test_complexity_validator.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.test import override_settings
 
 
 from django.contrib.auth.forms import UserCreationForm
@@ -24,6 +25,31 @@ class ComplexityValidatorTest(TestCase):
             'username': 'Hana',
             'password1': 'toMay!23',
             'password2': 'toMay!23',
+        })
+        userForm.full_clean()
+        self.assertFalse(
+            userForm.has_error('password1') or userForm.has_error('password2')
+        )
+
+
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {
+            'NAME': 'password_policies.password_validation.ComplexityValidator',
+            'OPTIONS':{
+                'min_char_types': 2,
+                'min_numeric_chars':2,
+                'min_uppercase_chars':0,
+                'min_lowercase_chars':2,
+                'min_symbol_chars':0,
+            },
+        }
+    ])
+    def test_lowercase_number_2種字元就好(self):
+        hana = User.objects.create_user('Hana')
+        userForm = UserCreationForm({
+            'username': 'Hana',
+            'password1': 'tomay123',
+            'password2': 'tomay123',
         })
         userForm.full_clean()
         self.assertFalse(
