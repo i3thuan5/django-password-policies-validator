2058a8902acf7e634a40ec484a6efc7dbcd375ef
Zuer <hong@ithuan.tw>
2022-12-21 16:15:22 +0800

    加時間檢查。

--- password_policies/middleware.py
+++ password_policies/middleware.py
@@ -1,11 +1,26 @@
+from datetime import timedelta
+from django.conf import settings
+from django.shortcuts import redirect
+from django.utils import timezone
+
+
 class PasswordExpirationMiddleware:
     def __init__(self, get_response):
-        self.get_response = get_response
         # One-time configuration and initialization.
+        self.get_response = get_response
+        self.expiration_days = timedelta(
+            days=getattr(settings, 'PASSWORD_EXPIRATION_DAYS', 90.0)
+        )
 
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
+
+        if request.user.is_authenticated:
+            latest_password_record = request.user.password_records.latest('date')
+            if (timezone.now() - latest_password_record.date) \
+                > self.expiration_days:
+                return redirect('admin:password_change')
 
         response = self.get_response(request)
 
