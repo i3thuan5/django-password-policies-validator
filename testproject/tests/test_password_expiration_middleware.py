a1eb07081ca8e4377cd5856ad705085158edc241
Zuer <hong@ithuan.tw>
2022-12-21 15:20:38 +0800

    試驗lia̍h方向。

---
+++ testproject/tests/test_password_expiration_middleware.py
@@ -0,0 +1,12 @@
+
+class PasswordExpirationMiddleware(TestCase):
+	def test_新使用者login正常(self):
+		self.fail()
+	def test_新使用者過90工攏無改密碼_會到重設密碼ê網頁(self):
+		self.fail()
+	def test_新使用者過90工攏無改密碼_tī登入頁正常顯示(self):
+		self.fail()
+	def test_使用者改過密碼_tī90工內正常登入(self):
+		self.fail()
+	def test_options設做180_tī90工正常登入(self):
+		self.fail()
