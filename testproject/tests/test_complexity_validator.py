3d5824d7b68ff3c966868bfbb1d6f91e0cab8993
Zuer <hong@ithuan.tw>
2022-12-21 10:48:53 +0800

    試驗有成功。

--- testproject/tests/test_complexity_validator.py
+++ testproject/tests/test_complexity_validator.py
@@ -6,12 +6,14 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 
 class ComplexityValidatorTest(TestCase):
-	def test_lowercase_number(self):
+	def test_lowercase_number_至少4種字元(self):
 		hana = User.objects.create_user('Hana')
 		userForm = UserCreationForm({
 			'username': 'Hana',
 			'password1': 'tomay123',
 			'password2': 'tomay123',
 		})
-		with self.assertRaises(ValidationError, userForm.full_clean) as error:
-			'大寫、小寫、數字、特殊符號中至少4種字元' in error.objects[0]
+		userForm.full_clean()
+		self.assertTrue(
+			userForm.has_error('password1') or userForm.has_error('password2')
+		)
