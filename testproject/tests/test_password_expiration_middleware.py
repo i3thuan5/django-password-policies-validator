917abbc7ccbd86f629207ec7e7e79b1f75af5740
Zuer <hong@ithuan.tw>
2022-12-21 15:37:24 +0800

    補settings

--- testproject/tests/test_password_expiration_middleware.py
+++ testproject/tests/test_password_expiration_middleware.py
@@ -7,7 +7,10 @@ from django.test import override_settings
 
 
 class PasswordExpirationMiddleware(TestCase):
-	def test_新使用者login正常(self):
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ])
+    def test_新使用者login正常(self):
         user_form = UserCreationForm({
             'username': 'Hana',
             'password1': 'tomay123',
@@ -22,60 +25,70 @@ class PasswordExpirationMiddleware(TestCase):
         response = self.client.get(reverse('admin:index'))
         self.assertEqual(response.status_code, 200)
 
-	def test_新使用者過90工攏無改密碼_tī登入頁正常顯示(self):
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ])
+    def test_新使用者過90工攏無改密碼_tī登入頁正常顯示(self):
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
         ):
-	        user_form = UserCreationForm({
-	            'username': 'Hana',
-	            'password1': 'tomay123',
-	            'password2': 'tomay123',
-	        })
-	        user_form.full_clean()
-	        hana = user_form.save()
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=90)
         ):
-	        response = self.client.get(reverse('admin:login'))
-	        self.assertEqual(response.status_code, 200)
+            response = self.client.get(reverse('admin:login'))
+            self.assertEqual(response.status_code, 200)
 
-	def test_新使用者過90工攏無改密碼_會到重設密碼ê網頁(self):
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ])
+    def test_新使用者過90工攏無改密碼_會到重設密碼ê網頁(self):
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
         ):
-	        user_form = UserCreationForm({
-	            'username': 'Hana',
-	            'password1': 'tomay123',
-	            'password2': 'tomay123',
-	        })
-	        user_form.full_clean()
-	        hana = user_form.save()
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=90)
         ):
-	        self.client.post(reverse('admin:login'),{
-	            'username': 'Hana',
-	            'password': 'tomay123',
-	        })
-	        response = self.client.get(reverse('admin:index'))
-	        self.assertEqual(response.status_code, 302)
-	        self.assertEqual(response.url, reverse('admin:setpassword'))
-	def test_使用者改過密碼_tī90工內正常登入(self):
+            self.client.post(reverse('admin:login'),{
+                'username': 'Hana',
+                'password': 'tomay123',
+            })
+            response = self.client.get(reverse('admin:index'))
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.url, reverse('admin:setpassword'))
+
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ])
+    def test_使用者改過密碼_tī90工內正常登入(self):
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
         ):
-	        user_form = UserCreationForm({
-	            'username': 'Hana',
-	            'password1': 'tomay123',
-	            'password2': 'tomay123',
-	        })
-	        user_form.full_clean()
-	        hana = user_form.save()
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=30)
@@ -90,32 +103,34 @@ class PasswordExpirationMiddleware(TestCase):
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=90)
         ):
-	        self.client.post(reverse('admin:login'),{
-	            'username': 'Hana',
-	            'password': 'pawli456',
-	        })
-	        response = self.client.get(reverse('admin:index'))
-	        self.assertEqual(response.status_code, 200)
-
-	def test_options設做180_tī90工正常登入(self):
+            self.client.post(reverse('admin:login'),{
+                'username': 'Hana',
+                'password': 'pawli456',
+            })
+            response = self.client.get(reverse('admin:index'))
+            self.assertEqual(response.status_code, 200)
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ], PASSWORD_EXPIRATION_DAYS=180.0)
+    def test_options設做180_tī90工正常登入(self):
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
         ):
-	        user_form = UserCreationForm({
-	            'username': 'Hana',
-	            'password1': 'tomay123',
-	            'password2': 'tomay123',
-	        })
-	        user_form.full_clean()
-	        hana = user_form.save()
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
         with patch(
             'django.utils.timezone.now',
             return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=90)
         ):
-	        self.client.post(reverse('admin:login'),{
-	            'username': 'Hana',
-	            'password': 'tomay123',
-	        })
-	        response = self.client.get(reverse('admin:index'))
-	        self.assertEqual(response.status_code, 200)
+            self.client.post(reverse('admin:login'),{
+                'username': 'Hana',
+                'password': 'tomay123',
+            })
+            response = self.client.get(reverse('admin:index'))
+            self.assertEqual(response.status_code, 200)
