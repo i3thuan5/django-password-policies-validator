85f03238625acc5108d045db224bc429d1820f41
Zuer <hong@ithuan.tw>
2022-12-21 14:31:50 +0800

    試驗先lia̍h方向。

---
+++ testproject/tests/test_minimum_reset_interval_validator.py
@@ -0,0 +1,9 @@
+
+class MinimumResetIntervalValidatorTest(TestCase):
+	def test_改了隨改密碼_會擋起來(self):
+		self.fail()
+	def test_過1工後_ētàng改(self):
+		self.fail()
+	def test_options設做2_過1工後改密碼_會擋起來(self):
+		self.fail()
+		
