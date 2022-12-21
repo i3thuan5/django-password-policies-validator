d862044954066d782f8b9dcd512fbbff1f05e7f1
Zuer <hong@ithuan.tw>
2022-12-21 17:03:04 +0800

    別ê adminsite試驗有成功。

--- testproject/tests/test_password_expiration_middleware.py
+++ testproject/tests/test_password_expiration_middleware.py
@@ -8,6 +8,9 @@ from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import SetPasswordForm
 from django.urls import reverse
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.urls import path
 
 
 class PasswordExpirationMiddleware(TestCase):
@@ -154,3 +157,40 @@ class PasswordExpirationMiddleware(TestCase):
             })
             response = self.client.get(reverse('admin:index'))
             self.assertEqual(response.status_code, 200)
+
+    @override_settings(MIDDLEWARE=settings.MIDDLEWARE + [
+        'password_policies.middleware.PasswordExpirationMiddleware',
+    ], ROOT_URLCONF='tests.test_password_expiration_middleware')
+    def test_admin是別ê名(self):
+        with patch(
+            'django.utils.timezone.now',
+            return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)
+        ):
+            user_form = UserCreationForm({
+                'username': 'Hana',
+                'password1': 'tomay123',
+                'password2': 'tomay123',
+            })
+            user_form.full_clean()
+            hana = user_form.save()
+            hana.is_active = True
+            hana.is_staff = True
+            hana.save()
+        with patch(
+            'django.utils.timezone.now',
+            return_value=datetime(2022, 12, 21, tzinfo=timezone.utc)+timedelta(days=90)
+        ):
+            self.client.post(reverse('autai:login'),{
+                'username': 'Hana',
+                'password': 'tomay123',
+            })
+            response = self.client.get(reverse('autai:index'))
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.url, reverse('autai:password_change'))
+
+
+autai_site=AdminSite(name='autai')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('autai/', autai_site.urls),
+]
