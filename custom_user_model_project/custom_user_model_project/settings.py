2f8c8937886357be6f74646688070faee2680154
Tia i X570-ACE <hong@ithuan.tw>
2025-01-24 11:16:00 +0800

    加app

--- custom_user_model_project/custom_user_model_project/settings.py
+++ custom_user_model_project/custom_user_model_project/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INSTALLED_APPS += ['accounts']
+AUTH_USER_MODEL = "accounts.CustomUser"
